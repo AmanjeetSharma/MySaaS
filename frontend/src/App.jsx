01e23e814dced3b8c067540b5b1bb91959b7ceab
Amanjeet Sharma <amansharma23503@gmail.com>
2026-04-12 02:17:45 +0530

    feat: setting up with frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app-container flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h1>Welcome to the React App!</h1>
+
+    </div>
+  )
+}
+
+export default App
